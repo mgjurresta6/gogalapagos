3f67955c0a23b2861363b1db3ad87f93e5219eb4
Made <mgj.urresta@yavirac.edu.ec>
2025-03-26 17:31:38 -0500

    terminando front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ComplementoPruebaComponent } from './complemento-prueba/complemento-prueba.component';
 import { PdfComponent } from './pdf/pdf.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
 export const routes: Routes = [
     { path: '', component: ComplementoPruebaComponent },
-   { path: 'pdf', component: PdfComponent } //Ejemplo de ruta de componente
+   { path: 'pdf', component: PdfComponent }, //Ejemplo de ruta de componente
 ];
